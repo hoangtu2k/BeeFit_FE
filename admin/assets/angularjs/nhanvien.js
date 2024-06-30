f3cf4d6dd89c88c24e3e17ade0dcf450bc0845ce
phunghoangtu <thocodeonline@gmail.com>
2024-06-30 21:48:33 +0700

    tk , top san pham ban chay

--- admin/assets/angularjs/nhanvien.js
+++ admin/assets/angularjs/nhanvien.js
@@ -1,6 +1,8 @@
 window.NhanVienController = function ($scope, $http, $rootScope, AuthService) {
     document.getElementById('header-wrapper').style.display = 'block';
    
+    const url = "http://localhost:8080/api/employee";
+
   
 };
   
